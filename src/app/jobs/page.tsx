c4e1317d3f746ca8a68d0f673540b95bc51d078f
garrett hutson <hutsongarrett@gmail.com>
2023-04-14 08:11:57 -0400

    more type error fixes

--- src/app/jobs/page.tsx
+++ src/app/jobs/page.tsx
@@ -1,17 +1,65 @@
 
 
-import JobCard from '../components/JobCard'
+import React from 'react'
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+import Image from 'next/image';
 
 type Props = {}
-function page({ }: Props) {
 
+async function JobCard({ }: Props) {
 
+  const allJobs = await prisma.jobs.findMany()
+  await prisma.$disconnect();
   return (
- <div className='h-[80%] w-full flex overflow-auto justify-center mt-48 gap-2'>
-<JobCard />
- </div>
+    <>
+      {
+        allJobs.map((job, i) => (
+          <div key={i} className='border-black border-2 min-w-48 h-fit p-4 rounded-lg flex flex-col gap-4 '>
+            <div >{job.title}</div>
+            <div>{job.email}</div>
+            <div>{job.description}</div>
+          
+           <div className='flex w-full'>
+           <Image
+              src={`data:image/jpeg;base64,${job.img}`}
+              className='rounded-xl'
+              width={150}
+              height={150}
+              alt="Picture of the author"
+            />
+            <div className='ml-8'>
+       <div>{job.location}</div>
+       <div>{job.Budget}</div>
+            </div>
+
+           </div>
+     
+
+          </div>
+        ))
+      }
+    </>
+
+
 
   )
 }
 
-export default page
+
+
+// import JobCard from '../components/JobCard'
+
+// type Props = {}
+// function page({ }: Props) {
+
+
+//   return (
+//  <div className='h-[80%] w-full flex overflow-auto justify-center mt-48 gap-2'>
+// <JobCard />
+//  </div>
+
+//   )
+// }
+
+// export default page
