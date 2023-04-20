256f744f3867788626c5d68b41462a037ac6d1b6
garrett hutson <hutsongarrett@gmail.com>
2023-04-20 10:26:53 -0400

    get

--- src/app/api/jobs/route.tsx
+++ src/app/api/jobs/route.tsx
@@ -2,7 +2,7 @@ import { PrismaClient } from '@prisma/client'
 import { NextResponse } from 'next/server';
 const prisma = new PrismaClient()
 
-export async function GET(){
+export async function GET(request:Request){
 
      
     await prisma.$connect();
